0bb780cdbc44374760e9f0b0aa6076abddeb1571
a-keyter <106679998+a-keyter@users.noreply.github.com>
2024-01-24 15:53:20 +0000

    Update home.py

--- home.py
+++ home.py
@@ -48,7 +48,7 @@ if submit and problem != "":
     if local_test == False:
         openaikey = user_openai
     os.environ['OPENAI_API_KEY'] = openaikey
-    llm = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0.7)
+    llm = OpenAI(model_name="gpt-3.5-turbo-0613", temperature=0.7)
     
     customers_template = PromptTemplate(
         input_variables= ["problem"],
